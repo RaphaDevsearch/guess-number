e4b5bcbad4bfc1d020e0af8cd97f5ccfe9e25b03
RaphaDevsearch <mizarapahelpro@gmail.com>
2025-05-28 12:38:33 +0530

    update prototype of my game

--- guessing.py
+++ guessing.py
@@ -8,18 +8,31 @@ class NumberGuessing:
         self.trie = 0
         self.life = 4
         
-    def check_guess(self, guess):
-        print('guessing ...',guess)
+    def is_right(self, guess):
         return guess == self.secretNumber
     
     def play(self):
         guess = int(input("guess : ")) # input user
         
-        while not self.check_guess(guess):
-            guess = int(input("guess again : "))
+        while True :
+            if not self.is_right(guess):
+                guess = int(input("guess again ")) # input user
+                
+                self.trie += 1
+                if self.trie >= self.life -1 :
+                    print("Lose :(")
+                    break
+            
+            else :
+                print("Bravo :)")
+                break
+            
+        
+        # while not self.check_guess(guess) and self.life > 1:
+        #     guess = int(input(f"guess again :{self.life} left : "))
+        #     self.life -= 1
         
 
-        print("ok you know ...")
 
 if __name__ == "__main__":
     g = NumberGuessing(2,6)
